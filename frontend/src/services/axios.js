28d397d5154b76fa0d2121f9657722d6196c4b22
Zuwairiya-1147 <sulaikzuwairiya@gmail.com>
2026-05-25 11:15:03 +0530

    fix api url

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: '/api' });
+const API = axios.create({
+  baseURL: 'https://ai-resume-analyzer-backend-ohld.onrender.com/'
+});
 
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem('token');
